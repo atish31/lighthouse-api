e1ce180a47d3ac29ef5df466f23f61fa3a7351e5
atish <atish@nineleaps.com>
2022-11-29 16:14:29 +0530

    Delete metrics file if it exists, changes in controllers/lighthouse file

--- controllers/lighthouse.js
+++ controllers/lighthouse.js
@@ -1,9 +1,15 @@
+const fs = require('fs');
+
 function getLightHouseScore(req, res) {
   const { exec } = require('child_process');
   const readJsonFile = require('../services/json-output');
   const url = req.query.url;
-  const fileName = 'metrics/metrics.json'
-  const lighthouseShellCommand = `lighthouse ${url} --chrome-flags='--headless' --output json --output-path ${fileName}`
+  const fileName = 'metrics/metrics.json';
+  const lighthouseShellCommand = `lighthouse ${url} --chrome-flags='--headless' --output json --output-path ${fileName}`;
+
+  if (fs.existsSync('./metrics/metrics.json')) {
+    fs.unlinkSync('./metrics/metrics.json');
+  }
 
   exec(lighthouseShellCommand, (error, shellOut, shellError) => {
     if(error) {
